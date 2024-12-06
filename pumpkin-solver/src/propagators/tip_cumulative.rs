cf93f18e450e69f4146222aedc61d3f199b9083a
Tip ten Brink <75669206+tiptenbrink@users.noreply.github.com>
2024-12-06 15:24:44 +0100

    now use setup based on events

--- pumpkin-solver/src/propagators/tip_cumulative.rs
+++ pumpkin-solver/src/propagators/tip_cumulative.rs
@@ -1,6 +1,10 @@
 #![allow(unused)]
 
-use std::collections::HashMap;
+use std::{
+    collections::{HashMap, HashSet},
+    i32,
+    num::NonZero,
+};
 
 use crate::{
     basic_types::{Inconsistency, PropagationStatusCP},
@@ -15,20 +19,69 @@ use crate::{
 pub(crate) struct CumulativePropagator<const SIZE: usize> {
     start_times: Box<[DomainId; SIZE]>,
     processing_times: Box<[u32; SIZE]>,
+    // INVARIANT: usage may not be 0 and must fit within i32
     resource_usages: Box<[u32; SIZE]>,
     resource_capacity: u32,
 }
 
+/// An event where an activity with index `activity` becomes either active (then usage_change > 0) or inactive (then usage_change < 0)
+struct Event {
+    activity: usize,
+    time: i32,
+    usage_change: i32,
+}
+
+struct Interval {
+    start: i32,
+    end_inclusive: i32,
+    // total usage of activities
+    usage: i64,
+    // active activities
+    active: Vec<usize>,
+}
+
+fn process_event(
+    active: &mut HashSet<usize>,
+    event: &Event,
+    usage: i64,
+    capacity: u32,
+    time: i32,
+) -> Result<i64, TimeTableInconsistency> {
+    // We can avoid using HashSet by maybe sorting on removal time, then in log n we can find and remove it
+    if event.usage_change > 0 {
+        let _ = active.insert(event.activity);
+    } else if event.usage_change < 0 {
+        let _ = active.remove(&event.activity);
+    } else {
+        unreachable!("Usages should never be zero!")
+    }
+
+    let new_usage = usage + (event.usage_change as i64);
+
+    if new_usage > (capacity as i64) {
+        Err(TimeTableInconsistency {
+            time,
+            activities: active.iter().copied().collect(),
+        })
+    } else {
+        Ok(new_usage)
+    }
+}
+
 impl<const SIZE: usize> CumulativePropagator<SIZE> {
-    fn build_resource_profile_from_bounds(&self, bounds: &BoundsSlice) -> ResourceProfile<SIZE> {
+    fn build_resource_profile_from_bounds(
+        &self,
+        bounds: &BoundsSlice,
+    ) -> Result<ResourceProfile, TimeTableInconsistency> {
         self.build_resource_profile(|v| (bounds[v].0, bounds[v].1))
     }
 
     fn build_resource_profile<F: Fn(usize) -> (i32, i32)>(
         &self,
         bound_fn: F,
-    ) -> ResourceProfile<SIZE> {
-        let mut compulsory_parts = Vec::<CompulsoryPart>::with_capacity(SIZE);
+    ) -> Result<ResourceProfile, TimeTableInconsistency> {
+        // At most SIZE*2 events, so let's pre-allocate
+        let mut events: Vec<Event> = Vec::with_capacity(SIZE * 2);
 
         for i in 0..self.start_times.len() {
             let processing_time = self.processing_times[i];
@@ -36,18 +89,34 @@ impl<const SIZE: usize> CumulativePropagator<SIZE> {
 
             let end_lower_bound = start_lower_bound + (processing_time as i32);
 
-            let compulsory_part = if start_upper_bound < end_lower_bound {
-                CompulsoryPart::Interval((start_upper_bound, end_lower_bound))
+            let usage = self.resource_usages[i];
+
+            if start_upper_bound < end_lower_bound {
+                events.push(Event {
+                    activity: i,
+                    time: start_upper_bound,
+                    usage_change: (usage as i32),
+                });
+                events.push(Event {
+                    activity: i,
+                    time: end_lower_bound,
+                    usage_change: -(usage as i32),
+                });
             } else {
-                CompulsoryPart::Unknown
+                // We don't know anything in this case
             };
-
-            compulsory_parts.push(compulsory_part);
         }
 
-        ResourceProfile {
-            compulsory_parts: compulsory_parts.try_into().unwrap(),
+        if events.len() == 0 {
+            return Ok(ResourceProfile {
+                intervals: Vec::new(),
+            });
         }
+
+        // Sorts events in ascending order by time
+        events.sort_by_key(|e| e.time);
+
+        ResourceProfile::from_events(events, self.resource_capacity)
     }
 }
 
@@ -59,10 +128,11 @@ enum CompulsoryPart {
 
 type BoundsSlice = [(i32, i32)];
 
-struct ResourceProfile<const SIZE: usize> {
-    compulsory_parts: Box<[CompulsoryPart; SIZE]>,
+struct ResourceProfile {
+    intervals: Vec<Interval>,
 }
 
+/// An inconsistency that indicates the resource was overloaded at time `time` by activities in `activities` being active
 struct TimeTableInconsistency {
     time: i32,
     // Using the index into the propagator
@@ -95,71 +165,68 @@ impl TimeTableInconsistency {
 
 type Histogram = HashMap<i32, (u32, Vec<usize>)>;
 
-impl<const SIZE: usize> ResourceProfile<SIZE> {
-    fn time_table_consistency_check(
-        &self,
-        usages: &[u32; SIZE],
-        capacity: u32,
-    ) -> Result<Histogram, TimeTableInconsistency> {
-        // Computed from scratch
+impl ResourceProfile {
+    /// INVARIANT: events must be sorted in ascending order
+    fn from_events(mut events: Vec<Event>, capacity: u32) -> Result<Self, TimeTableInconsistency> {
+        let mut intervals = Vec::new();
+        let mut active = HashSet::new();
+        let mut usage = 0;
+        let mut event = events.pop().unwrap();
 
-        // Alternatively we could create events "becomes active at t", "stops being active at t" and sort them (O (n log n))
-        // Then go through it and increment/decremen total usage based on these events
-        // Here we make a hashmap histogram instead, this allows O(n * max_t_interval) check I think
-        // However it makes generating explanations more difficult (doesn't gives us as much flexibility to choose the time)
+        // preconditions: events are sorted, so next_event.time >= previous_event.time
+        while events.len() > 0 {
+            let interval_start = event.time;
 
-        let mut histogram: Histogram = HashMap::new();
+            usage = process_event(&mut active, &event, usage, capacity, interval_start)?;
 
-        for (i, compulsory_part) in self.compulsory_parts.iter().enumerate() {
-            let (start, end) = match compulsory_part {
-                CompulsoryPart::Interval((start, end)) => (*start, *end),
-                CompulsoryPart::Unknown => continue,
-            };
+            // We checked len > 0, so we can unwrap
+            let mut next_event = events.pop().unwrap();
+            while events.len() > 0 && next_event.time == interval_start {
+                // We know next_event.time == interval_start, so we don't advance the interval
+                usage = process_event(&mut active, &next_event, usage, capacity, interval_start)?;
 
-            let usage = usages[i];
+                // We checked len > 0, so we can unwrap
+                next_event = events.pop().unwrap();
+                debug_assert!(next_event.time >= interval_start);
+            }
 
-            debug_assert!(start <= end);
+            // Postcondition: events.len() == 0 \/ next_event.time != interval_start
+            if (next_event.time != interval_start) {
+                // We have an interval end!
 
-            // For every time we are active, we increase the usage
-            // This part is suboptimal because we depend on the size of the time values
-            for t in start..=end {
-                // We put this in separate scope so that we no longer have mutable reference to histogram entries
-                let ok = {
-                    let (prev_usage, active_activities) =
-                        histogram.entry(t).or_insert_with(|| (0, Vec::new()));
-                    active_activities.push(i);
-                    // If usage exceeds capacity, infeasible
-                    if *prev_usage + usage > capacity {
-                        false
-                    } else {
-                        *prev_usage += usage;
-                        true
-                    }
+                // Next event starts at next_event.time, so previous one ends one time earlier
+                let interval_end = next_event.time - 1;
+
+                let interval = Interval {
+                    start: interval_start,
+                    end_inclusive: interval_end,
+                    usage,
+                    // Copy all the ones active
+                    active: active.iter().copied().collect(),
                 };
 
-                if !ok {
-                    // We know it exists
-                    let inconsistency = TimeTableInconsistency {
-                        time: t,
-                        activities: histogram.remove(&t).unwrap().1,
-                    };
-                    return Err(inconsistency);
-                }
+                intervals.push(interval);
+                event = next_event;
+            } else {
+                // We know that events.len() == 0 in this case and same time as interval_start
+                // So we cannot determine the interval end
+                // No more should be active, every start should have associated end
+                debug_assert!(active.len() == 0);
+                debug_assert!(usage == 0);
+
+                // No more interval has to be created, because they should have all ended
             }
         }
 
-        Ok(histogram)
+        Ok(Self { intervals })
     }
 
     fn time_table_filtering(
         &self,
-        histogram: Histogram,
         bounds: &BoundsSlice,
         capacity: u32,
     ) -> Result<(), TimeTableInconsistency> {
-        for (lb, ub) in bounds {
-            
-        }
+        for (lb, ub) in bounds {}
 
         Ok(())
     }
@@ -172,13 +239,11 @@ impl<const SIZE: usize> Propagator for CumulativePropagator<SIZE> {
 
     fn debug_propagate_from_scratch(&self, context: PropagationContextMut) -> PropagationStatusCP {
         // Is it right that we don't know what event caused the propagator to be called? Useful for global propagator?
-        let profile: ResourceProfile<SIZE> = self.build_resource_profile(|v| {
-            let var = &self.start_times[v];
-            (context.lower_bound(var), context.upper_bound(var))
-        });
-
-        let histogram: Histogram = profile
-            .time_table_consistency_check(&self.resource_usages, self.resource_capacity)
+        let profile = self
+            .build_resource_profile(|v| {
+                let var = &self.start_times[v];
+                (context.lower_bound(var), context.upper_bound(var))
+            })
             .map_err(|e| {
                 e.into_prop_conj(
                     self.start_times.as_slice(),
@@ -193,13 +258,11 @@ impl<const SIZE: usize> Propagator for CumulativePropagator<SIZE> {
         &mut self,
         context: &mut PropagatorInitialisationContext,
     ) -> Result<(), PropositionalConjunction> {
-        let profile: ResourceProfile<SIZE> = self.build_resource_profile(|v| {
-            let var = &self.start_times[v];
-            (context.lower_bound(var), context.upper_bound(var))
-        });
-
-        let _ = profile
-            .time_table_consistency_check(&self.resource_usages, self.resource_capacity)
+        let profile = self
+            .build_resource_profile(|v| {
+                let var = &self.start_times[v];
+                (context.lower_bound(var), context.upper_bound(var))
+            })
             .map_err(|e| {
                 e.into_prop_conj(
                     self.start_times.as_slice(),
